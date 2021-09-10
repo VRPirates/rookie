49551784264ac5202568e7bc467b3275ef998cde
William Swartwood <thisisyourknife@gmail.com>
2021-09-10 04:55:02 -0400

    Started copying new updater form for asking for game updates, will work on it this weekend.

---
+++ NewGames.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndroidSideloader
+{
+    public partial class NewGames : Form
+    {
+        public NewGames()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void UpdateVerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YesUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CurrentVerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SkipUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
