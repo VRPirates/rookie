e7bf9ce9a755ed158615d6c87b406f603abfb718
Chax <nerfnichtmist@gmail.com>
2022-09-13 17:37:09 +0200

    Fixed Icons

--- UsernameForm.Designer.cs
+++ UsernameForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UsernameForm));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -73,6 +74,7 @@
             this.Controls.Add(this.textBox1);
             this.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::AndroidSideloader.Properties.Settings.Default, "BackColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(459, 139);
             this.MinimumSize = new System.Drawing.Size(459, 139);
             this.Name = "UsernameForm";
