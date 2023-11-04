6edc9cf7e98f4da5ad2671b33639cfca9e348585
Chax <chaxprivate@gmail.com>
2023-11-04 16:19:15 +0100

    Change not authorized message to be more clear on what to do
    Some additional info

--- MainForm.cs
+++ MainForm.cs
@@ -812,7 +812,7 @@ namespace AndroidSideloader
                 this.Invoke(() =>
                 {
                     Text = "Device Not Authorized";
-                    DialogResult dialogResult = FlexibleMessageBox.Show(Program.form, "Device not authorized, be sure to authorize computer on device.", "Not Authorized", MessageBoxButtons.RetryCancel);
+                    DialogResult dialogResult = FlexibleMessageBox.Show(Program.form, "Please check inside your headset for ADB DEBUGGING prompt/notification, check the box \"Always allow from this computer.\" and hit OK.", "Not Authorized", MessageBoxButtons.RetryCancel);
                     if (dialogResult == DialogResult.Retry)
                     {
                         devicesbutton.PerformClick();
