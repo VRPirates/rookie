a5b0e60acc4faad43080c7dc06ae1954ce28057d
fenopy <fenopie@gmail.com>
2024-11-10 10:50:44 -0600

    chore: bump rclone to 1.68.1

--- Sideloader/GetDependencies.cs
+++ Sideloader/GetDependencies.cs
@@ -124,7 +124,7 @@ namespace AndroidSideloader
                 Application.Exit();
             }
 
-            string wantedRcloneVersion = "1.67.0";
+            string wantedRcloneVersion = "1.68.1";
             bool rcloneSuccess = false;
 
             rcloneSuccess = downloadRclone(wantedRcloneVersion, false);
