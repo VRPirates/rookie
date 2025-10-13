0d42e3dea0b88715107e62f0318d112ce87e3605
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-10-13 15:14:35 -0600

    Add dedicated 'StartupOptions' class to replace separate fields in MainForm

---
+++ StartupOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace AndroidSideloader
+{
+    public sealed class StartupOptions
+    {
+        public bool OfflineMode { get; set; } = false;
+
+        public bool RcloneUpdatesDisabled { get; set; }
+
+        public bool NoAppCheck { get; set; } = false;
+    }
+}
